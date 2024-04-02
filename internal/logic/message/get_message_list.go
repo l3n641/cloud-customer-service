91b42a65ecc5b4863804e688f581dd99f1b808a2
l3n641 <371522155@qq.com>
2024-04-02 10:34:31 +0800

    fix bug 查看消息列表

--- internal/logic/message/get_message_list.go
+++ internal/logic/message/get_message_list.go
@@ -12,10 +12,11 @@ func (s *sMessage) GetMessageList(ctx context.Context, in *model.MessageListInpu
 	out := &model.ClientMessageListOutput{
 		Page: in.Page,
 		Size: in.Size,
+		List: make([]model.ClientMessageItem, 0),
 	}
 	m := dao.Messages.Ctx(ctx).Page(in.Page, in.Size).OrderDesc(dao.Messages.Columns().Id).Where(dao.Messages.Columns().TicketId, in.TicketId)
 	if !in.LastMessageId.IsEmpty() {
-		m = m.Where(fmt.Sprintf("%s > %d", dao.Messages.Columns().Id, in.LastMessageId.Int()))
+		m = m.Where(fmt.Sprintf("%s < %d", dao.Messages.Columns().Id, in.LastMessageId.Int()))
 	}
 	err := m.ScanAndCount(&out.List, &out.Total, false)
 	return out, err
