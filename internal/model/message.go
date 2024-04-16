e6585eeb04f0a54c91da06d8afe85afad0e2676d
l3n641 <371522155@qq.com>
2024-04-16 09:28:27 +0800

    fix bug ClientMessageItem type

--- internal/model/message.go
+++ internal/model/message.go
@@ -46,9 +46,9 @@ type ClientMessageItem struct {
 	Id         int       `json:"id" description:"消息id"`
 	CreateAt   time.Time `json:"create_at" description:"消息创建实际"`
 	SenderType int       `json:"sender_type" description:"发送类型 1-顾客发送给客服,2-客服发送给客人"`
-	MsgType    string    `json:"msg_type" description:"消息类型: 1-文本消息"`
+	MsgType    int       `json:"msg_type" description:"消息类型: 1-文本消息"`
 	Content    string    `json:"content" description:"消息内容"`
-	IsRead     string    `json:"is_read" description:"消息是否被对方读取:0-未读，1-已读"`
+	IsRead     int       `json:"is_read" description:"消息是否被对方读取:0-未读，1-已读"`
 	ReadTime   time.Time `json:"read_time" description:"读取消息的时间"`
 	RefererUrl string    `json:"referer_url" description:"客户发送消息时候所在的页面"`
 }
